efc600057f4629c25ad9fa83acdeb2cf17cad9c1
Jorge Wanderley <jorgew.reis@outlook.com>
2020-08-12 12:00:56 -0300

    corrigindo bugs

--- google-scripts/code-auxiliar.js
+++ google-scripts/code-auxiliar.js
@@ -1,26 +1,25 @@
-// Funções auxiliares
-var app = SpreadsheetApp;
-var bd = app.getActiveSpreadsheet().getSheetByName("BancodeDados");
-var atualID = Session.getActiveUser().getEmail();
-
 // Declara uma mensagem de BOAS VINDAS personalizada
 function msgBemVindo() {
+    var app = SpreadsheetApp.getActiveSpreadsheet();;
     var infoTime = Utilities.formatDate(new Date(), "GMT-03:00", "HH");
     var message = "";
     var user = buscaUser();
 
     if (infoTime <= 12) {
-        message = "Bom dia" + user + "!";
+        message = "Bom dia " + user + "!";
     } else if (infoTime < 18) {
-        message = "Boa tarde" + user + "!";
+        message = "Boa tarde " + user + "!";
     } else {
-        message = "Boa noite" + user + "!";
+        message = "Boa noite " + user + "!";
     }
-    app.getActiveSpreadsheet().toast(message, "Olá ", 7)
+    app.toast(message, "Olá ", 7)
 }
 
 // Retorna o NOME DE USUÁRIO cadastrado no banco de dados baseado no e-mail de login
 function buscaUser() {
+    var app = SpreadsheetApp.getActiveSpreadsheet();;
+    var bd = app.getSheetByName("BancodeDados");
+    var atualID = Session.getActiveUser().getEmail();
     var atualUser = "Outro usuário";
 
     for (var i = 0; i < 15; i++) {
@@ -33,6 +32,9 @@ function buscaUser() {
 
 // Retorna o NÍVEL DE SEGURANÇA cadastrado no banco de dados baseado no e-mail de login
 function verificaSeg() {
+    var app = SpreadsheetApp.getActiveSpreadsheet();;
+    var bd = app.getSheetByName("BancodeDados");
+    var atualID = Session.getActiveUser().getEmail();
     var nivelUser = { code: 1, desc: "Básico" };
 
     for (var i = 0; i < 15; i++) {
--- google-scripts/code-openSheets.js
+++ google-scripts/code-openSheets.js
@@ -1,101 +1,118 @@
 // Funções executadas ao abrir planilha
-
 function onOpen() {
     msgBemVindo();
 }
 
-var app = SpreadsheetApp;
 
+// -------------------------------------------------------------------------------------
+// Funções somente de abertura de Planilhas
 function abrirAgenda() {
-    var sheet = app.SpreadsheetApp.getSheetByName("Agenda").activate();
+    var app = SpreadsheetApp.getActiveSpreadsheet();
+    var sheet = app.getSheetByName("Agenda").activate();
     sheet.getRange("A8").activate();
 }
 
 function abrirZonaMand() {
-    var sheet = app.SpreadsheetApp.getSheetByName("ZonaMand").activate();
+    var app = SpreadsheetApp.getActiveSpreadsheet();
+    var sheet = app.getSheetByName("ZonaMand").activate();
     sheet.getRange("F10").clearContent();
 }
 
 function abrirCalcMulta() {
-    var sheet = app.SpreadsheetApp.getSheetByName("CalcMulta").activate();
+    var app = SpreadsheetApp.getActiveSpreadsheet();
+    var sheet = app.getSheetByName("CalcMulta").activate();
     sheet.getRange("C10:G10").clearContent();
 }
 
 function abrirLotPol() {
-    var sheet = app.SpreadsheetApp.getSheetByName("LotPol").activate();
+    var app = SpreadsheetApp.getActiveSpreadsheet();
+    var sheet = app.getSheetByName("LotPol").activate();
     sheet.getRange("F12").clearContent();
 }
 
 function abrirOficios() {
-    app.SpreadsheetApp.getSheetByName("Ofícios").activate();
+    var app = SpreadsheetApp.getActiveSpreadsheet();
+    app.getSheetByName("Ofícios").activate();
 }
 
 function abrirJuntada() {
-    app.SpreadsheetApp.getSheetByName("Juntada").activate();
+    var app = SpreadsheetApp.getActiveSpreadsheet();
+    app.getSheetByName("Juntada").activate();
 }
 
 function abrirVistas() {
-    app.SpreadsheetApp.getSheetByName("Vistas").activate();
+    var app = SpreadsheetApp.getActiveSpreadsheet();
+    app.getSheetByName("Vistas").activate();
 }
 
 function abrirVidConf() {
-    var sheet = app.SpreadsheetApp.getSheetByName("VidConf").activate();
+    var app = SpreadsheetApp.getActiveSpreadsheet();
+    var sheet = app.getSheetByName("VidConf").activate();
     sheet.getRange("B11").activate();
 }
 
 function abrirCartPrec() {
-    var sheet = app.SpreadsheetApp.getSheetByName("CartPrec").activate();
+    var app = SpreadsheetApp.getActiveSpreadsheet();
+    var sheet = app.getSheetByName("CartPrec").activate();
     sheet.getRange("B11").activate();
 }
 
 function abrirReusPresos() {
-    app.SpreadsheetApp.getSheetByName("ReusPresos").activate();
+    var app = SpreadsheetApp.getActiveSpreadsheet();
+    app.getSheetByName("ReusPresos").activate();
 }
 
 function abrirBancoDados() {
-    var sheet = app.SpreadsheetApp.getSheetByName("BancodeDados");
+    var app = SpreadsheetApp.getActiveSpreadsheet();
+    var sheet = app.getSheetByName("BancodeDados");
     var nivel = verificaSeg();
     if (nivel.code >= 5) { //Acesso níveis 5 e 6 - Restrict / Extreme
         sheet.activate();
     } else {
-        app.getActiveSpreadsheet().toast('Seu nível de segurança é ' + nivel.code + ' - ' + nivel.desc + '. Você não tem permissão de acesso ao Banco de Dados. Permitido apenas para níveis 5 e 6', "Acess denied!", 10);
+        app.toast('Seu nível de segurança é ' + nivel.code + ' - ' + nivel.desc + '. Você não tem permissão de acesso ao Banco de Dados. Permitido apenas para níveis 5 e 6', "Acess denied!", 10);
     }
 }
 
 function abrirVersoes() {
-    var sheet = app.SpreadsheetApp.getSheetByName("Versoes");
+    var app = SpreadsheetApp.getActiveSpreadsheet();
+    var sheet = app.getSheetByName("Versoes");
     var nivel = verificaSeg();
     if (nivel.code >= 5) { //Acesso níveis 5 e 6 - Restrict / Extreme
         sheet.activate();
     } else {
-        app.getActiveSpreadsheet().toast('Seu nível de segurança é ' + nivel.code + ' - ' + nivel.desc + '. Você não tem permissão de acesso ao Controle de Versionamento. Permitido apenas para níveis 5 e 6', "Acess denied!", 10);
+        app.toast('Seu nível de segurança é ' + nivel.code + ' - ' + nivel.desc + '. Você não tem permissão de acesso ao Controle de Versionamento. Permitido apenas para níveis 5 e 6', "Acess denied!", 10);
     }
 }
 
 function abrirCalendar() {
-    var sheet = app.SpreadsheetApp.getSheetByName("Calendar");
+    var app = SpreadsheetApp.getActiveSpreadsheet();
+    var sheet = app.getSheetByName("Calendar");
     var nivel = verificaSeg();
     if (nivel >= 4) { //Acesso níveis 4 a 6 - Exclusive - Extreme
         sheet.activate();
     } else {
-        app.getActiveSpreadsheet().toast('Seu nível de segurança é ' + nivel.code + ' - ' + nivel.desc + '. Você não tem permissão de acesso aos Ajustes de Calendário. Permitido apenas para níveis 4 a 6', "Acess denied!", 10);
+        app.toast('Seu nível de segurança é ' + nivel.code + ' - ' + nivel.desc + '. Você não tem permissão de acesso aos Ajustes de Calendário. Permitido apenas para níveis 4 a 6', "Acess denied!", 10);
     }
 }
 
 function abrirProdut() {
-    app.SpreadsheetApp.getSheetByName("Produt").activate();
+    var app = SpreadsheetApp.getActiveSpreadsheet();
+    app.getSheetByName("Produt").activate();
 }
 
 function abrirProcessos() {
-    app.SpreadsheetApp.getSheetByName("Processos").activate();
+    var app = SpreadsheetApp.getActiveSpreadsheet();
+    app.getSheetByName("Processos").activate();
 }
 
 function voltarHome() {
-    app.SpreadsheetApp.getSheetByName("Home").activate();
+    var app = SpreadsheetApp.getActiveSpreadsheet();
+    app.getSheetByName("Home").activate();
 }
 
 function voltarHomeSeg() {
-    app.SpreadsheetApp.getSheetByName("Home").activate();
+    var app = SpreadsheetApp.getActiveSpreadsheet();
+    app.getSheetByName("Home").activate();
     app.getSheetByName("Versoes").hideSheet();
     app.getSheetByName("BancodeDados").hideSheet();
     app.getSheetByName("Calendar").hideSheet();
